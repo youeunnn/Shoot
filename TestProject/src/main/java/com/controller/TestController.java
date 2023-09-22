111af9be1511b34e3c5faf533e60f080c808dda1
acorn <acorn@DESKTOP-I9POGS4>
2023-09-22 18:04:03 +0900

    main push

--- TestProject/src/main/java/com/controller/TestController.java
+++ TestProject/src/main/java/com/controller/TestController.java
@@ -28,6 +28,9 @@ public class TestController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("wannago");
+		System.out.println("wannago23");
+		System.out.println("wannago3");
+		System.out.println("wannago4");
 	}
 
 	/**
